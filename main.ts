b800af896cd51be6eebfc8540150f34b1f6f64e7
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2022-06-09 16:20:17 +0900

    JINS MEMEから取得したメッセージをそのままGUIまで転送する

--- main.ts
+++ main.ts
@@ -3,22 +3,40 @@ import { serveFile } from "https://deno.land/std@0.142.0/http/file_server.ts";
 
 import { handleMessage } from "./memedata.ts";
 
+const guiSockets: WebSocket[] = [];
+
 async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
-  if (url.pathname === "/meme") {
+  if (url.pathname === "/") {
     let response, socket: WebSocket;
     try {
       ({ response, socket } = Deno.upgradeWebSocket(req));
     } catch {
       return new Response("request isn't trying to upgrade to websocket.");
     }
-    socket.onopen = () => console.log("socket opened");
-    socket.onmessage = handleMessage;
-    socket.onerror = (e) => console.log("socket errored:", e);
-    socket.onclose = () => console.log("socket closed");
+    // socket.onopen = () => console.log("socket opened");
+    socket.onmessage = handleMessage(guiSockets);
+    // socket.onerror = (e) => console.log("socket errored:", e);
+    // socket.onclose = () => console.log("socket closed");
     return response;
   } else if (url.pathname === "/gui") {
     return await serveFile(req, "gui.html");
+  } else if (url.pathname === "/gui-socket") {
+    let response, socket: WebSocket;
+    try {
+      ({ response, socket } = Deno.upgradeWebSocket(req));
+    } catch {
+      return new Response("request isn't trying to upgrade to websocket.");
+    }
+    // socket.onopen = () => console.log("socket opened");
+    // socket.onmessage = handleMessage;
+    // socket.onerror = (e) => console.log("socket errored:", e);
+    socket.onclose = () => {
+      const idx = guiSockets.findIndex((x) => x === socket);
+      guiSockets.splice(idx, 1);
+    };
+    guiSockets.push(socket);
+    return response;
   } else {
     return new Response("Not Found", { status: 404 });
   }
--- memedata.ts
+++ memedata.ts
@@ -1,5 +1,8 @@
-export const handleMessage = (msg: MessageEvent<unknown>) => {
-  const rawJson = msg.data as string;
-  const data = JSON.parse(rawJson);
-  console.log(`yaw:${data.yaw}, pitch:${data.pitch}, roll:${data.roll}`);
+export const handleMessage = (guiSockets: WebSocket[]) => {
+  return (msg: MessageEvent<unknown>) => {
+    const rawJson = msg.data as string;
+    const data = JSON.parse(rawJson);
+    console.log(`yaw:${data.yaw}, pitch:${data.pitch}, roll:${data.roll}`);
+    guiSockets.forEach((sock) => sock.send(rawJson));
+  };
 };
