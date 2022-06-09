fc416afaaeb0a9f720cf94eeb9d6d35279be283c
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2022-06-09 15:38:39 +0900

    GUI用のHTMLを返す

--- main.ts
+++ main.ts
@@ -1,19 +1,27 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.142.0/http/file_server.ts";
 
 import { handleMessage } from "./memedata.ts";
 
-function handler(req: Request): Response {
-  let response, socket: WebSocket;
-  try {
-    ({ response, socket } = Deno.upgradeWebSocket(req));
-  } catch {
-    return new Response("request isn't trying to upgrade to websocket.");
+async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url);
+  if (url.pathname === "/meme") {
+    let response, socket: WebSocket;
+    try {
+      ({ response, socket } = Deno.upgradeWebSocket(req));
+    } catch {
+      return new Response("request isn't trying to upgrade to websocket.");
+    }
+    socket.onopen = () => console.log("socket opened");
+    socket.onmessage = handleMessage;
+    socket.onerror = (e) => console.log("socket errored:", e);
+    socket.onclose = () => console.log("socket closed");
+    return response;
+  } else if (url.pathname === "/gui") {
+    return await serveFile(req, "gui.html");
+  } else {
+    return new Response("Not Found", { status: 404 });
   }
-  socket.onopen = () => console.log("socket opened");
-  socket.onmessage = handleMessage;
-  socket.onerror = (e) => console.log("socket errored:", e);
-  socket.onclose = () => console.log("socket closed");
-  return response;
 }
 
 serve(handler, { hostname: "0.0.0.0", port: 19999 });
