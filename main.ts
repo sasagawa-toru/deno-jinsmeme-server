266012674336de7d06d4c4786067ca9b95d86402
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2022-07-22 17:46:32 +0900

    接続時にキャリブレーションする機能を追加

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.142.0/http/file_server.ts";
 
 const guiSockets: WebSocket[] = [];
+let origYaw = 0;
+let origPitch = 0;
 
 async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
@@ -15,21 +17,32 @@ async function handler(req: Request): Promise<Response> {
       } catch {
         return new Response("request isn't trying to upgrade to websocket.");
       }
+      let firstMessageReceived = false;
       socket.onmessage = (msg: MessageEvent<unknown>) => {
         const rawJson = msg.data as string;
         const data = JSON.parse(rawJson);
+        if (!firstMessageReceived) {
+          firstMessageReceived = true;
+          origYaw = data.yaw;
+          origPitch = data.pitch;
+        }
         console.log(`yaw:${data.yaw}, pitch:${data.pitch}, roll:${data.roll}`);
-        guiSockets.forEach((sock) => sock.send(rawJson));
+        data.yaw = data.yaw - origYaw;
+        data.pitch = data.pitch - origPitch;
+        const newJson = JSON.stringify(data);
+        guiSockets.forEach((sock) => sock.send(newJson));
       };
       return response;
     }
 
     // HTML画面を表示(staticコンテンツ)
+
     case "/gui": {
       return await serveFile(req, "gui.html");
     }
 
     // HTML画面からの接続先(WebSocket)
+
     case "/gui-socket": {
       let response, socket: WebSocket;
       try {
@@ -46,6 +59,7 @@ async function handler(req: Request): Promise<Response> {
     }
 
     // 画像表示(staticコンテンツ)
+
     case "/face.png": {
       return await serveFile(req, "face.png");
     }
